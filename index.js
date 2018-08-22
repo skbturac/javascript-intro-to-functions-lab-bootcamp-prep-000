fffee3ab6f36b6c4922f243e5079bf313dbb3433
Suheyl Turac <skbsturac@live.com>
2018-08-22 19:16:16 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,36 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string){
+  return string.toLowerCase()
+}
+function logShout(string){
+  return string.toMixedCase()
+}
+function logWhisper(string){
+  return string.toLowerCase()
+}
+
+var logWhisper = 'hello'
+var logShout = 'Hello'
+var toUpperCase = 'HELLO!'
+var toLowerCase ='hello!'
+var toMixedCase = 'Hi there!'
+
+lowercase.toLowerCase() === lowercase
+uppercase.toUpperCase() === uppercase
+logShout.toUpperCase() === mixedCase
+logWhisper.toLowerCase() === lowercase
+mixedCase.toMixedCase() === mixedCase 
+
+function sayHiToGrandma(string) {
+  if ((string)===uppercase) {
+    return 'YES INDEED!'
+  }
+  else if ((string)===lowercase) {
+    return "I can't hear you!"
+  }
+  else if ((string)==="I love you, Grandma.") {
+    return "I love you, too."
+  }
+  }
