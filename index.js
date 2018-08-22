84857a0b4493fbb00c57d64a7425b77fd8e4c64b
Suheyl Turac <skbsturac@live.com>
2018-08-22 19:44:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function shout(string) {
 function whisper(string){
   return string.toLowerCase()
 }
-function logShout(string) {
+function logShout() {
   console.log('HELLO!');
 }
 function logWhisper(){
